7744e188a0fd6fa51bd8d47dfe940156f80c7181
tanyaka <litium@yandex.ru>
2023-08-30 02:12:16 +0300

    изменен анализ лайков

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -9,7 +9,8 @@ function Card({ card, onCardClick, onCardLike, onDeleteClick }) {
     const isOwn = card.owner === currentUser._id
 
     // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
-    const isLiked = card.likes.some((i) => i._id === currentUser._id)
+    // const isLiked = card.likes.some((i) => i._id === currentUser._id)
+    const isLiked = card.likes.some((i) => i === currentUser._id)
 
     // Создаём переменную, которую после зададим в `className` для кнопки лайка
     const cardLikeButtonClassName = `element__like ${
